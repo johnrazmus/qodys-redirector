1afbeacaba6bb06e696f546f10d5c05c701f95ec
johnrazmus <john@qody.co>
2014-04-29 10:41:47 -0500

    homepage now has welcome message

--- pages/home/content.php
+++ pages/home/content.php
@@ -1,7 +1,11 @@
 <?php
 $plugin_data = $this->GetPluginData();
 
-$doc_url = $_GET['doc_url'] ? $_GET['doc_url'] : 'http://docs.qody.co/plugins/specific-products/alejandro-qodys-redirector/';
+$fields = array();
+$fields['p'] = 'qodys-redirector';
+$fields['blog'] = get_bloginfo('url');
+
+$frame_url = 'http://razmus.net/frames/plugin_home.php?'.http_build_query( $fields );
 ?>
 
 <div class="wrap qody_only_area">
@@ -10,13 +14,12 @@ $doc_url = $_GET['doc_url'] ? $_GET['doc_url'] : 'http://docs.qody.co/plugins/sp
 		<h1><?php echo $plugin_data['Name']; ?> <small>version <?php echo $plugin_data['Version']; ?></small></h1>
 	</div>
 	
-	
 	<?php $this->Helper('postman')->DisplayMessages(); ?>
 	
 	<div class="row-fluid" style="height:100%;">
 		<div class="span12" style="height:100%;">
 			
-			<iframe src="<?php echo $doc_url; ?>" style="height:80%; width:100%; border:none;"></iframe>
+			<iframe src="<?php echo $frame_url; ?>" style="height:80%; width:100%; border:none;"></iframe>
 			
 		</div>
 	</div>
